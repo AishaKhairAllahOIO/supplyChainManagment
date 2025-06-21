8f8158b5dfa1d492718c3ecba91a8631fbac765a
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-21 11:00:45 +0300

    Define a constructor to check constraints and give initial values.

--- src/ProductInformation.java
+++ src/ProductInformation.java
@@ -5,4 +5,16 @@ public class ProductInformation
     private double productPrice;
     private int productQuantity;
 
+    public ProductInformation(int productId,String productName,double productPrice,int productQuantity)
+    {
+        if (productPrice<0) throw new IllegalArgumentException("Price cannot be negative.");
+        if (productQuantity<0||productQuantity>1000)
+            throw new IllegalArgumentException("Quantity must be between 0 and 1000.");
+
+        this.productId=productId;
+        this.productName=productName;
+        this.productPrice=productPrice;
+        this.productQuantity=productQuantity;
+    }
+
 }
