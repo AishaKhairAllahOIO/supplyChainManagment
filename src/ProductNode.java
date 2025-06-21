6602c10434fada15228f91758620331540626517
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-21 11:16:56 +0300

    Add ProductNode class to represent AVL tree nodes for product storage

---
+++ src/ProductNode.java
@@ -0,0 +1,14 @@
+public class ProductNode {
+    ProductInformation product;
+    ProductNode left;
+    ProductNode right;
+    int height;
+
+    public ProductNode(ProductInformation product)
+    {
+        this.product = product;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+    }
+}
