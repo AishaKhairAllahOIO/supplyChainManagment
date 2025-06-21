3986a26bebff2a680f88986ef04739556b5b757b
AishaKhairAllahOIO <aishakhairallah@gmail.com>
2025-06-21 11:04:35 +0300

    Definition of modification and access functions

--- src/ProductInformation.java
+++ src/ProductInformation.java
@@ -17,4 +17,38 @@ public class ProductInformation
         this.productQuantity=productQuantity;
     }
 
+    public int getProductId()
+    {
+        return productId;
+    }
+    public String getProductName()
+    {
+        return productName;
+    }
+    public double getProductPrice()
+    {
+        return productPrice;
+    }
+    public int getProductQuantity()
+    {
+        return productQuantity;
+    }
+
+    public void setProductPrice(double productPrice)
+    {
+        if (productPrice<0) throw new IllegalArgumentException("Price cannot be negative.");
+        this.productPrice=productPrice;
+    }
+    public void setProductQuantity(int productQuantity)
+    {
+        if(productQuantity<0||productQuantity>1000)
+            throw new IllegalArgumentException("Quantity must be between 0 and 1000.");
+        this.productQuantity=productQuantity;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "[ID: " + productId + "] " + productName + " | Price: " + productPrice + " | Quantity: " + productQuantity;
+    }
 }
